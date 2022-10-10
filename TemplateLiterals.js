d34e79066057d84e57af9e6e02d9ebc8bb40532c
Fernando Henrique Castro Bastos <fernando0henrique@hotmail.com>
2022-10-10 20:53:09 -0300

    Template Literals
    I'll show to you how to use template literals.
    
    `   Text ${  }       `

---
+++ TemplateLiterals.js
@@ -0,0 +1,4 @@
+const name = 'Fernando'
+const birthday = '17/10/1989'
+console.log(`My name is ${name}! and my birthday is ${birthday}, then i am 32 years old!`)
+console.log(`This Js file has been created just for demonstrate how to use "Template Literals! Copy this code and open in your console to see that!"`)
